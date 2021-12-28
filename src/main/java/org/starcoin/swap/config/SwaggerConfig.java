1dcecd5272ebf60c19353f439eababe38ddb0b7f
fountainchen <fountainchen@yahoo.com>
2021-12-28 12:02:26 +0800

    [deploy]update swagger-ui config

--- src/main/java/org/starcoin/swap/config/SwaggerConfig.java
+++ src/main/java/org/starcoin/swap/config/SwaggerConfig.java
@@ -20,20 +20,19 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfig {
     @Bean
     public Docket docket() {
-        return new Docket(DocumentationType.OAS_30).groupName("v2")
+        return new Docket(DocumentationType.OAS_30).groupName("swap-stat")
                 .apiInfo(apiInfo()).enable(true)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.starcoin.swap.controller"))
-                .paths(regex("/v2/.*"))
+                .paths(regex("/.*"))
                 .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("starcoin swap-stat-api doc")
-                .description("Library supports all available Starcoin Swap Stat API calls for all available Starcoin Networks for \n" +
-                        "explorer.starcoin.org")
-                .contact(new Contact("author", "fountainX", "suoyuan@yahoo.com"))
+                .description("Library supports all available Starcoin Swap Stat API calls for all available Starcoin Networks" )
+                .contact(new Contact("author", "fountainchen", "fountainchen@yahoo.com"))
                 .version("1.0")
                 .build();
     }
