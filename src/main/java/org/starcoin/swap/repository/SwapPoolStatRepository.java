e9aba3857fa51c44a4ca99ff4efb13e8be06d04c
fountainchen <fountainchen@yahoo.com>
2022-04-13 23:19:32 +0800

    [pool/token]update token and pool last day stat

--- src/main/java/org/starcoin/swap/repository/SwapPoolStatRepository.java
+++ src/main/java/org/starcoin/swap/repository/SwapPoolStatRepository.java
@@ -8,19 +8,19 @@ import org.starcoin.swap.entity.SwapPoolStat;
 import java.util.List;
 
 public interface SwapPoolStatRepository extends JpaRepository<SwapPoolStat, String> {
-    @Query(value = "select first_token_name, second_token_name, now() as ts, sum(volume_amount) as volume_amount, sum(volume) as volume, sum(tvl_a_amount) as tvl_a_amount, sum(tvl_a) as tvl_a, sum(tvl_b_amount) as tvl_b_amount, sum(tvl_b) as tvl_b  from main.pool_swap_day_stat group by first_token_name, second_token_name", nativeQuery = true)
-    List<SwapPoolStat> sum();
+    @Query(value = "select * from pool_swap_day_stat where ts = current_date - 1", nativeQuery = true)
+    List<SwapPoolStat> lastDayStat();
 
-    @Query(value = "select first_token_name, second_token_name, now() as ts, sum(volume_amount) as volume_amount, sum(volume) as volume, sum(tvl_a_amount) as tvl_a_amount, sum(tvl_a) as tvl_a, sum(tvl_b_amount) as tvl_b_amount, sum(tvl_b) as tvl_b  from main.pool_swap_day_stat where (first_token_name=:token_x_name and second_token_name=:token_y_name) group by first_token_name, second_token_name", nativeQuery = true)
-    SwapPoolStat sumByToken(@Param("token_x_name") String tokenXName, @Param("token_y_name") String tokenYName);
+    @Query(value = "select * from pool_swap_day_stat where ts = (current_date - 1) and first_token_name=:token_x_name and second_token_name=:token_y_name", nativeQuery = true)
+    SwapPoolStat lastStatByToken(@Param("token_x_name") String tokenXName, @Param("token_y_name") String tokenYName);
 
     @Query(value = "select * from pool_swap_day_stat where (first_token_name=:token_x_name and second_token_name=:token_y_name) or (first_token_name=:token_y_name and second_token_name=:token_x_name) order by volume limit :count offset :offset", nativeQuery = true)
     List<SwapPoolStat> find(@Param("token_x_name") String tokenXName, @Param("token_y_name") String tokenYName,
                             @Param("offset") int offset,
                             @Param("count") int count);
 
-    @Query(value = "select first_token_name, second_token_name, now() as ts, sum(volume_amount) as volume_amount, sum(volume) as volume, sum(tvl_a_amount) as tvl_a_amount, sum(tvl_a) as tvl_a, sum(tvl_b_amount) as tvl_b_amount, sum(tvl_b) as tvl_b  from main.pool_swap_day_stat where (first_token_name=:token_name or second_token_name=:token_name) group by first_token_name, second_token_name", nativeQuery = true)
-    List<SwapPoolStat> sumByOneToken(@Param("token_name") String tokenName);
+    @Query(value = "select * from pool_swap_day_stat where ts = (current_date - 1) and (first_token_name=:token_name or second_token_name=:token_name)", nativeQuery = true)
+    List<SwapPoolStat> lastStatByOneToken(@Param("token_name") String tokenName);
 
     @Query(value = "select * from pool_swap_day_stat where (first_token_name=:token_name or second_token_name=:token_name) order by volume limit :count offset :offset", nativeQuery = true)
     List<SwapPoolStat> findAll(@Param("offset") int offset,
