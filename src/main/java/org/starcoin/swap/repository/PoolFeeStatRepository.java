df5c3ae5161b3b28c4bca09bc4a23770667a9eec
fountainchen <fountainchen@yahoo.com>
2022-04-18 08:51:52 +0800

    [pool]update token pool fee token order

--- src/main/java/org/starcoin/swap/repository/PoolFeeStatRepository.java
+++ src/main/java/org/starcoin/swap/repository/PoolFeeStatRepository.java
@@ -8,7 +8,7 @@ import org.starcoin.swap.entity.PoolFeeStat;
 import java.util.List;
 
 public interface PoolFeeStatRepository extends JpaRepository<PoolFeeStat, String> {
-    @Query(value = "select * from pool_fee_day_stat where (first_token_name=:token_x_name and second_token_name=:token_y_name) or (first_token_name=:token_y_name and second_token_name=:token_x_name) limit :count offset :offset", nativeQuery = true)
+    @Query(value = "select * from pool_fee_day_stat where (first_token_name=:token_x_name and second_token_name=:token_y_name) or (first_token_name=:token_y_name and second_token_name=:token_x_name) order by ts desc limit :count offset :offset", nativeQuery = true)
     List<PoolFeeStat> findAll(@Param("token_x_name") String tokenXName, @Param("token_y_name") String tokenYName,
                               @Param("offset") int offset,
                               @Param("count") int count);
