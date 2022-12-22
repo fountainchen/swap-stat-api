131aa686a5ca9816cb486f9cc3f423b711acb962
fountainchen <fountainchen@yahoo.com>
2022-12-22 17:05:08 +0800

    [swap]update pool stat query order

--- src/main/java/org/starcoin/swap/repository/SwapPoolStatRepository.java
+++ src/main/java/org/starcoin/swap/repository/SwapPoolStatRepository.java
@@ -14,7 +14,7 @@ public interface SwapPoolStatRepository extends JpaRepository<SwapPoolStat, Stri
     @Query(value = "select * from pool_swap_day_stat where ts = (current_date - 1) and first_token_name=:token_x_name and second_token_name=:token_y_name", nativeQuery = true)
     SwapPoolStat lastStatByToken(@Param("token_x_name") String tokenXName, @Param("token_y_name") String tokenYName);
 
-    @Query(value = "select * from pool_swap_day_stat where (first_token_name=:token_x_name and second_token_name=:token_y_name) or (first_token_name=:token_y_name and second_token_name=:token_x_name) order by volume limit :count offset :offset", nativeQuery = true)
+    @Query(value = "select * from pool_swap_day_stat where (first_token_name=:token_x_name and second_token_name=:token_y_name) or (first_token_name=:token_y_name and second_token_name=:token_x_name) order by ts desc limit :count offset :offset", nativeQuery = true)
     List<SwapPoolStat> find(@Param("token_x_name") String tokenXName, @Param("token_y_name") String tokenYName,
                             @Param("offset") int offset,
                             @Param("count") int count);
@@ -22,7 +22,7 @@ public interface SwapPoolStatRepository extends JpaRepository<SwapPoolStat, Stri
     @Query(value = "select * from pool_swap_day_stat where ts = (current_date - 1) and (first_token_name=:token_name or second_token_name=:token_name)", nativeQuery = true)
     List<SwapPoolStat> lastStatByOneToken(@Param("token_name") String tokenName);
 
-    @Query(value = "select * from pool_swap_day_stat where (first_token_name=:token_name or second_token_name=:token_name) order by volume limit :count offset :offset", nativeQuery = true)
+    @Query(value = "select * from pool_swap_day_stat where (first_token_name=:token_name or second_token_name=:token_name) order by ts desc limit :count offset :offset", nativeQuery = true)
     List<SwapPoolStat> findAll(@Param("offset") int offset,
                                @Param("count") int count,
                                @Param("token_name") String tokenName);
